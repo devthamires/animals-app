290f054a1262181b00edf0891239f8772043f8d3
Thamires Garcia <thamires.garcia@habber.com>
2022-05-20 22:26:52 +0100

    footer: create and desing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AnimalDetailsComponent } from './animal-details/animal-details.component';
+import { AnimalsListComponent } from './animals-list/animals-list.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        redirectTo: 'animals',
+        pathMatch: 'full',
+      },
+      {
+        path: 'animals',
+        component: AnimalsListComponent,
+      },
+      {
+        path: 'animals/:id',
+        component: AnimalDetailsComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
+import { AnimalDetailsModule } from './animal-details/animal-details.module';
+import { AnimalsListModule } from './animals-list/animals-list.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterModule } from './core/footer/footer.module';
+import { HeaderModule } from './core/header/header.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AnimalDetailsModule,
+    AnimalsListModule,
+    RouterModule,
+    HttpClientModule,
+    HeaderModule,
+    FooterModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
